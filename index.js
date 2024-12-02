00cb14c9d07eaa20e41b3907a1f7c293fd56a1a8
Roman Gurtenko <cpr.korzh@gmail.com>
2024-12-03 04:49:55 +0900

    First commit with a basic structure registration and authorization

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const router = require('./router/index');
+const database = require('./database');
+const errorMiddleware = require('./middlewares/error-middleware');
+
+
+const PORT = process.env.NODE_ENV === 'development'
+    ? process.env.DEV_PORT
+    : process.env.NODE_ENV === 'test'
+    ? process.env.TEST_PORT
+    : process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+app.use('/api', router);
+app.use(errorMiddleware);
+
+const start = async () => {
+    try {
+        database.sequelize.sync( { alter: true }).then(result=>{
+            console.log("Database synchronized successfully.");
+        })
+        .catch(err=> console.log(err));
+
+        app.listen(PORT, () => console.log(`Server is started on ${PORT} port`))
+    } catch (e) {
+        console.log(`Server is down. Reason: ${e}`);
+    }
+}
+
+start();
